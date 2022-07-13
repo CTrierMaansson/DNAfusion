e6c84e5659fbba8585b4825fd20cd652b61a3730
CTrierMaansson <ctm@clin.au.dk>
2022-07-13 13:21:43 +0200

    First commit

---
+++ R/ALKfusiondiscover_functions.R
@@ -0,0 +1,271 @@
+#' Detection of EML4-ALK variants
+#'
+#' This function looks for EML4-ALK mate pair reads in the BAM file.
+#'
+#' @param file Is the name of the BAM file.
+#' @param genome Is the reference genome. Can be either hg38 or hg19. Default = "hg38".
+#' @param mates Is the minimum number EML4-ALK mate pairs needed to be detected in order to call a variant. Default = 2.
+#' @return If EML4-ALK is detected a data.frame with soft-clipped reads representing EML4-ALK is returned. Otherwise "No EML4-ALK was detected" is returned
+#' @examples
+#' sample1 <- EML4_ALK_detection(file = "sample1.bam", genome = "hg38", mates = 2)
+#' sample2 <- EML4_ALK_detection(file = "sample2.bam", genome = "hg38", mates = 2)
+#' @export
+EML4_ALK_detection <- function(file, genome = "hg38", mates = 2){
+  `%ni%` <- Negate(`%in%`)
+  if (genome %ni% c("hg38", "hg19")){
+    return("ERROR: The reference genome has to be hg38 or hg19")
+  }
+  what <- c("mpos", "pos", "seq","cigar")
+  if (genome =="hg38"){
+    which <- GRanges(seqnames="chr2", IRanges(start = 42169353, end = 42332548))
+    param <- ScanBamParam(which = which, what = what)
+    bam <- scanBam(file = file, param = param)
+    reads <- data.frame(sequences = bam$`chr2:42169353-42332548`$seq,
+                        mate = bam$`chr2:42169353-42332548`$mpos,
+                        position = bam$`chr2:42169353-42332548`$pos,
+                        cigar = bam$`chr2:42169353-42332548`$cigar)
+    reads <- reads %>% filter(mate < 29921586 & mate > 29192774)
+  }
+  else{
+    which <- GRanges(seqnames="chr2", IRanges(start = 42396490, end = 42559688))
+    param <- ScanBamParam(which = which, what = what)
+    bam <- scanBam(file = file, param = param)
+    reads <- data.frame(sequences = bam$`chr2:42396490-42559688`$seq,
+                        mate = bam$`chr2:42396490-42559688`$mpos,
+                        position = bam$`chr2:42396490-42559688`$pos,
+                        cigar = bam$`chr2:42396490-42559688`$cigar)
+    reads <- reads %>% filter(mate < 30144477 & mate > 29415640)
+  }
+  if (length(reads$mate)<mates){
+    res <- "No EML4-ALK was detected"
+    return(res)
+  }
+  clip_reads <- reads %>% filter(cigar != "96M")
+  clip_reads <- clip_reads %>% filter(!grepl("D",cigar))
+  clip_reads <- clip_reads %>% filter(!grepl("I",cigar))
+  if (length(clip_reads$mate)<mates){
+    res <- "No EML4-ALK was detected"
+    return(res)
+  }
+  return(clip_reads)
+}
+
+#' Identification of EML4 breakpoint bases
+#'
+#' This function identifies the basepairs leading up to the EML4 breakpoint.
+#'
+#' @param reads Data.frame returned by EML4_ALK_detection().
+#' @param basepairs Number of basepairs identified from the EML4-ALK fusion. Default = 20.
+#' @return If EML4-ALK is detected, returns a table of identified EML4 basepairs with the number of corresponding reads for each sequence. Otherwise "No EML4-ALK was detected" is returned.
+#' @examples
+#' sample1 <- EML4_sequence(EML4_ALK_detection(file = "sample1.bam", genome = "hg38", mates = 2), basepairs = 20)
+#' sample2 <- EML4_sequence(EML4_ALK_detection(file = "sample2.bam", genome = "hg38", mates = 2), basepairs = 20)
+#' @export
+EML4_sequence <- function(reads, basepairs = 20){
+  if(class(reads) != "data.frame"){
+    if(reads == "No EML4-ALK was detected"){
+      return("No EML4-ALK was detected")
+    }
+    else{
+      return("reads must be a data.frame")
+    }
+  }
+  fun <- function(str) sub("\\M.*", "",str)
+  index1 <- sapply(reads$cigar,FUN = fun)
+  fun1 <- function(ind1){
+    if (length(ind1)>1){
+      return(NA)
+    }
+    else{
+      return(ind1)
+    }
+  }
+  fun <- function(ind){
+    splits <- strsplit(ind,split = "S")
+    splits <- lapply(splits, as.numeric)
+    splits_ind <- lapply(splits,FUN = fun1)
+    return(splits_ind)
+  }
+  index2 <- sapply(index1,FUN = fun)
+  reads$indeces <- index2
+  reads <- reads %>% filter(!is.na(indeces))
+  EML4_fun <- function(inp){
+    return(substring(inp$sequences,(inp$indeces-(basepairs-1)),inp$indeces))
+  }
+  char_df <-reads %>% dplyr::select(sequences,indeces)
+  EML4_seq <- apply(char_df, FUN = EML4_fun, MARGIN = 1)
+  EML4_tab <- table(EML4_seq)
+  return(EML4_tab)
+}
+
+#' Identification of ALK breakpoint bases
+#'
+#' This function identifies the basepairs following the ALK breakpoint.
+#'
+#' @param reads Data.frame returned by EML4_ALK_detection().
+#' @param basepairs Number of basepairs identified from the EML4-ALK fusion. Default = 20.
+#' @return If EML4-ALK is detected, returns a table of identified ALK basepairs with the number of corresponding reads for each sequence. Otherwise "No EML4-ALK was detected" is returned.
+#' @examples
+#' sample1 <- ALK_sequence(EML4_ALK_detection(file = "sample1.bam", genome = "hg38", mates = 2), basepairs = 20)
+#' sample2 <- ALK_sequence(EML4_ALK_detection(file = "sample2.bam", genome = "hg38", mates = 2), basepairs = 20)
+#' @export
+ALK_sequence <- function(reads, basepairs = 20){
+  if(class(reads) != "data.frame"){
+    if(reads == "No EML4-ALK was detected"){
+      return("No EML4-ALK was detected")
+    }
+    else{
+      return("reads must be a data.frame")
+    }
+  }
+  fun <- function(str) sub("\\M.*", "",str)
+  index1 <- sapply(reads$cigar,FUN = fun)
+  fun1 <- function(ind1){
+    if (length(ind1)>1){
+      return(NA)
+    }
+    else{
+      return(ind1)
+    }
+  }
+  fun <- function(ind){
+    splits <- strsplit(ind,split = "S")
+    splits <- lapply(splits, as.numeric)
+    splits_ind <- lapply(splits,FUN = fun1)
+    return(splits_ind)
+  }
+  index2 <- sapply(index1,FUN = fun)
+  reads$indeces <- index2
+  reads <- reads %>% filter(!is.na(indeces))
+  ALK_fun <- function(inp){
+    return(substring(inp$sequences,(inp$indeces+1),(inp$indeces+basepairs)))
+  }
+  char_df <- reads %>% dplyr::select(sequences,indeces)
+  ALK_seq <- apply(char_df, FUN = ALK_fun, MARGIN = 1)
+  ALK_tab <- table(ALK_seq)
+  return(ALK_tab)
+}
+
+#' EML4-ALK breakpoint
+#'
+#' This function identifies the genomic position in EML4 where the breakpoint has happened.
+#'
+#' @param reads Data.frame returned by EML4_ALK_detection().
+#' @return If EML4-ALK is detected, returns a table of genomic positions with the number of corresponding reads for each sequence. Otherwise "No EML4-ALK was detected" is returned.
+#' @examples
+#' sample1 <- break_position(EML4_ALK_detection(file = "sample1.bam", genome = "hg38", mates = 2))
+#' sample2 <- break_position(EML4_ALK_detection(file = "sample2.bam", genome = "hg38", mates = 2))
+#' @export
+break_position <- function(reads){
+  if(class(reads) != "data.frame"){
+    if(reads == "No EML4-ALK was detected"){
+      return("No EML4-ALK was detected")
+    }
+    else{
+      return("reads must be a data.frame")
+    }
+  }
+  fun <- function(str) sub("\\M.*", "",str)
+  index1 <- sapply(reads$cigar,FUN = fun)
+  fun1 <- function(ind1){
+    if (length(ind1)>1){
+      return(NA)
+    }
+    else{
+      return(ind1)
+    }
+  }
+  fun <- function(ind){
+    splits <- strsplit(ind,split = "S")
+    splits <- lapply(splits, as.numeric)
+    splits_ind <- lapply(splits,FUN = fun1)
+    return(splits_ind)
+  }
+  index2 <- sapply(index1,FUN = fun)
+  reads$indeces <- index2
+  reads <- reads %>% filter(!is.na(indeces))
+  reads$indeces <- as.numeric(reads$indeces)
+  break_pos <- reads$position + reads$indeces-1
+  break_pos_tab <- table(break_pos)
+  return(break_pos_tab)
+}
+
+
+#' Read depth at breakpoint
+#'
+#' This function identifies the read depth at the basepair before the breakpoint in EML4.
+#'
+#' @param file Is the name of the BAM file.
+#' @param reads Data.frame returned by EML4_ALK_detection().
+#' @return If EML4-ALK is detected a single integer corresponding to the read depth at the breakpoint is returned. Otherwise "No EML4-ALK was detected" is returned
+#' @examples
+#' sample1 <- break_position_depth(file = "sample1.bam",EML4_ALK_detection(file = "sample1.bam", genome = "hg38", mates = 2))
+#' sample2 <- break_position_depth(file = "sample2.bam",EML4_ALK_detection(file = "sample2.bam", genome = "hg38", mates = 2))
+#' @export
+break_position_depth <- function(file, reads){
+  if(class(reads) != "data.frame"){
+    if(reads == "No EML4-ALK was detected"){
+      return("No EML4-ALK was detected")
+    }
+    else{
+      return("reads must be a data.frame")
+    }
+  }
+  fun <- function(str) sub("\\M.*", "",str)
+  index1 <- sapply(reads$cigar,FUN = fun)
+  fun1 <- function(ind1){
+    if (length(ind1)>1){
+      return(NA)
+    }
+    else{
+      return(ind1)
+    }
+  }
+  fun <- function(ind){
+    splits <- strsplit(ind,split = "S")
+    splits <- lapply(splits, as.numeric)
+    splits_ind <- lapply(splits,FUN = fun1)
+    return(splits_ind)
+  }
+  index2 <- sapply(index1,FUN = fun)
+  reads$indeces <- index2
+  reads <- reads %>% filter(!is.na(indeces))
+  reads$indeces <- as.numeric(reads$indeces)
+  break_pos <- reads$position + reads$indeces-1
+  break_pos_tab <- table(break_pos)
+  stop_pos <- as.numeric(names(which.max(break_pos_tab)))
+  depth <- bamCoverage(file,GRanges(
+    seqnames = "chr2",IRanges(start=(stop_pos),end=stop_pos+1)),
+    mapqual=0,verbose=F)
+  return(max(depth[1]))
+}
+
+#' Complete EML4-ALK analysis
+#'
+#' This functions collects the results from the other functions of the package.
+#'
+#' @param file Is the name of the BAM file.
+#' @param genome Is the reference genome. Can be either hg38 or hg19. Default = "hg38".
+#' @param mates Is the minimum number EML4-ALK mate pairs needed to be detected in order to call a variant. Default = 2.
+#' @param basepairs Number of basepairs identified from the EML4-ALK fusion. Default = 20.
+#' @return A list object with clipped_reads corresponding to EML_ALK_detection(), last_EML4 corresponding to EML4_sequence(), first_ALK corresponding to ALK_sequence(), breakpoint corresponding to break_position(), and read_depth corresponding to break_position_depth().
+#' @examples
+#' sample1 <- EML4_ALK_analysis(file = "sample1.bam", genome = "hg38", mates = 2, basepairs = 20)
+#' sample2 <- EML4_ALK_analysis(file = "sample2.bam", genome = "hg38", mates = 2, basepairs = 20)
+#' @export
+EML4_ALK_analysis <- function(file, genome = "hg38", mates = 2, basepairs = 20){
+  res <- EML4_ALK_detection(file = file, genome = genome, mates = mates)
+  if(class(res) != "data.frame"){
+    return(res)
+  }
+  EML4 <- EML4_sequence(reads = res, basepairs = basepairs)
+  ALK <- ALK_sequence(reads = res, basepairs = basepairs)
+  position <- break_position(reads = res)
+  position_depth <- break_position_depth(file = file, reads = res)
+  return(list(clipped_reads = res,
+              last_EML4 = EML4,
+              first_ALK = ALK,
+              breakpoint = position,
+              read_depth = position_depth))
+}
+
